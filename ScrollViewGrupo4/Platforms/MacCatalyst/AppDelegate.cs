202d9ade663f152e219dfd039a08010e1b4fb2f3
kevin rosero <Kevin Rosero>
2022-11-21 07:38:27 -0500

    Primer commit

---
+++ ScrollViewGrupo4/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ScrollViewGrupo4;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
